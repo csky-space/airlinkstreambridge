ea02e440e0e81d25752adade1c1f8b0fce6f1b63
Sergey <s.zamaro@csky.space>
2025-03-07 15:51:20 +0300

    delete build from index

--- build/external/libs/uvgRTP/uvgrtp/uvgrtpConfig.cmake
+++
@@ -1,7 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/uvgrtpTargets.cmake")
-include(CMakeFindDependencyMacro)
-
-find_dependency(Threads)
-
-include("${CMAKE_CURRENT_LIST_DIR}/uvgrtpTargets.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/uvgrtpMacros.cmake")
