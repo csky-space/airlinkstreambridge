8846deaf130e7deeb99a102ae6ebb4f14c1969b0
Sergey Zamaro <firewall606@gmail.com>
2025-06-04 01:06:53 +0300

    update log server to regard API

--- logserver/logserver.go
+++ logserver/logserver.go
@@ -1,6 +1,8 @@
 package logserver
 
 import (
+	"io"
+	"log"
 	"net"
 	"os"
 	"sync"
@@ -46,4 +48,44 @@ func (b *BroadcastWriter) Write(p []byte) (n int, err error) {
 		}
 	}
 	return n, nil
+}
+
+func (bw *BroadcastWriter) StartTCPServer() {
+	address := "0.0.0.0:9000"
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Unable to start log server: %v\n", err)
+	}
+	log.Printf("TCP-server run at %s\n", address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error on accept: %v\n", err)
+			continue
+		}
+		log.Printf("Connected client: %s\n", conn.RemoteAddr())
+
+		bw.AddClient(conn)
+
+		go bw.handleClient(conn)
+	}
+}
+
+func (bw *BroadcastWriter) handleClient(conn net.Conn) {
+	defer func() {
+		log.Printf("Client disconnected: %s\n", conn.RemoteAddr())
+		bw.RemoveClient(conn)
+	}()
+
+	buf := make([]byte, 1)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("Read error %s: %v\n", conn.RemoteAddr(), err)
+			}
+			return
+		}
+	}
 }
